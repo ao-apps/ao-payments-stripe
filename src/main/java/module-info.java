8b13aaa125fd4bb7f5a7c4e6bab092e768e865f5
AO Industries, Inc <support@aoindustries.com>
2021-06-29 03:04:31 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,33 @@
+/*
+ * ao-payments-stripe - Provider for Stripe.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-payments-stripe.
+ *
+ * ao-payments-stripe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-payments-stripe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-payments-stripe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.aoapps.payments.stripe {
+	exports com.aoapps.payments.stripe;
+	// Direct
+	requires com.aoapps.collections; // <groupId>com.aoapps</groupId><artifactId>ao-collections</artifactId>
+	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+	requires com.aoapps.payments.api; // <groupId>com.aoapps</groupId><artifactId>ao-payments-api</artifactId>
+	requires org.apache.commons.lang3; // <groupId>org.apache.commons</groupId><artifactId>commons-lang3</artifactId>
+	requires stripe.java; // <groupId>com.stripe</groupId><artifactId>stripe-java</artifactId>
+	// Java SE
+	requires java.logging;
+}
